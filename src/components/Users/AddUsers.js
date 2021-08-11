8b9782ed478933b1d6c08cf2a0114b13c8fdb3be
thushu123 <thushara1994@gmail.com>
2021-08-11 23:03:42 +0530

    Adduser component

---
+++ src/components/Users/AddUsers.js
@@ -0,0 +1,65 @@
+import React, { useState } from 'react'
+import { Bootstrap, Grid, Row, Col, Container, Form, Button } from 'react-bootstrap';
+
+function AddUsers(props) {
+    const [enteredUsername, setUsername] = useState('');
+    const [enteredAge, setAge] = useState('');
+    const addUserHandler = (event) => {
+        event.preventDefault();
+        console.log(enteredUsername, enteredAge);
+    }
+    const userNameHandler = (event) => {
+        setUsername(event.target.value);
+    }
+    const ageHandler = (event) => {
+        setAge(event.target.value);
+    }
+    return (
+        <div>
+
+            {/* <Form  onSubmit={addUserHandler}>
+                        <div classNameName="form-group ">
+                            <label htmlFor="username">Username</label>
+                            <input type="text" id="username"></input>
+                        </div>
+                    
+                        <div className="form-group">
+                            <label htmlFor="age">Age </label>
+                            <input type="text" id="age"></input>
+                        </div>
+                        <Button type="submit">Add User</Button>
+                    </Form> */}
+
+            <div class="container">
+                <div class="row">
+                    <div className="col-md-6">
+                        <h2>Add User</h2>
+
+                        <form onSubmit={addUserHandler}>
+                            <div className="form-group row ">
+                                <label htmlFor="username" className="col-sm-2 col-form-label">Username</label>
+                                <div className="col-sm-10">
+                                    <input type="text" className="form-control" id="username" value={enteredUsername} placeholder="username" onChange={userNameHandler} />
+                                </div>
+                            </div>
+
+                            <div className="form-group row">
+                                <label htmlFor="age" className="col-sm-2 col-form-label">Age</label>
+                                <div className="col-sm-10">
+                                    <input type="text" className="form-control" id="age" value={enteredAge} placeholder="Age" onChange={ageHandler} />
+                                </div>
+                            </div>
+                            <Button type="submit">Add User</Button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+
+        </ div>
+
+
+
+    )
+}
+
+export default AddUsers
