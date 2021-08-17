6fde78bdbf2b941acccc1e513cb82359a65dd187
thushu123 <thushara1994@gmail.com>
2021-08-17 22:45:01 +0530

    updates

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import './App.css';
 import AddUsers from './components/Users/AddUsers';
 import UsersList from './components/Users/UsersList';
 import { useState } from 'react';
+// import Form from  './components/Users/'
 function App() {
   const [usersList, setUsersList] = useState([]);
 
   const addUserHandler = (uFname,uLname,uEmail, uAge) => {
 
     setUsersList((prevUsersList) => {
-      return [...prevUsersList, { firstname: uFname,lastname:uLname,email:uEmail, age: uAge, id: Math.random().toString() }]
+      return [...prevUsersList, { firstName: uFname,lastName:uLname,email:uEmail, age: uAge, id: Math.random().toString() }]
     })
      
   };
@@ -19,6 +20,8 @@ function App() {
       <AddUsers onAddUser={addUserHandler}></AddUsers>
       
       <UsersList users={usersList} ></UsersList> 
+      {/* <Form></Form> */}
+      
      
     </div>
   );
--- src/components/Users/AddUsers.js
+++ src/components/Users/AddUsers.js
@@ -1,76 +1,31 @@
 import React, { useState } from 'react'
 import { Bootstrap, Grid, Row, Col, Container, Form, Button, FormGroup, Label, Alert } from 'reactstrap';
 import { useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as Yup from "yup";
+const schema = Yup.object({
+    firstName: Yup.string().required('FirstName is Required').min(2),
+    lastName: Yup.string().required('LastName is Required').min(1),
+    email: Yup.string().email().required(),
+    age: Yup.number().positive().integer().
+    required("Please enter age.").typeError('Age must be a number'),
+    
+}).defined();
 function AddUsers(props) {
+    const { register, handleSubmit, formState: { errors } } = useForm({
+        resolver: yupResolver(schema),
+      });
     const [enteredFirstName, setFirstName] = useState('');
     const [enteredLastName, setLastName] = useState('')
     const [enteredEmail, setEmail] = useState('')
     const [enteredAge, setAge] = useState('');
     const [submited, setSubmited] = useState(false);
-    // const[register,addUserHandler]= useForm();
     const [valid, setValid] = useState(false)
-    const [errors, setErrors] = useState({
-        firstName: "",
-        lastName: "",
-        email: "",
-        age: ""
-    })
 
-
-    // const [users, setUsers] = useState([{
-    //     firstName: "",
-    //     lastName: "",
-    //     email: "",
-    //     age: ""
-    // }]);
     const addUserHandler = (event) => {
         event.preventDefault();
-        // if(enteredFirstName && enteredLastName && enteredEmail && enteredAge){
-        //     setValid(true);
-        // }
-        //firstname
+
         props.onAddUser(enteredFirstName, enteredLastName, enteredEmail, enteredAge);
-
-        if (!enteredFirstName) {
-            setValid(false);
-            setErrors({ ...errors, "firstName": "cannot be empty" });
-        }
-        if (typeof enteredFirstName !== "undefined") {
-            if (!enteredFirstName.match(/^[a-zA-Z]+$/)) {
-                setValid(false);
-                setErrors({ ...errors, "firstName": "only letters" });
-            }
-        }
-        //lastname
-        if (!enteredLastName) {
-            setValid(false);
-            setErrors({ ...errors, "lastName": "cannot be empty" });
-        }
-        if (typeof enteredLastlastName !== "undefined") {
-            if (!enteredLastName.match(/^[a-zA-Z]+$/)) {
-                setValid(false);
-                setErrors({ ...errors, "lastName": "only letters" });
-            }
-        }
-
-        //Email
-        if (!enteredEmail) {
-            setValid(false);
-            setErrors({ ...errors, "email": "cannot be empty" });
-        }
-        //Age
-
-        if (!enteredAge) {
-            setValid(false);
-            setErrors({ ...errors, "age": "cannot be empty" });
-        }
-        setSubmited(true);
-        // if(!errors){
-        //     props.onAddUser(enteredFirstName ,enteredLastName,enteredEmail, enteredAge);
-
-        // }
-        console.log('errors', errors);
-
         setFirstName('');
         setLastName('');
         setEmail('');
@@ -93,41 +48,45 @@ function AddUsers(props) {
         <div>
             <Container>
 
-
-                <Form onSubmit={addUserHandler} inline>
+                <Form onSubmit={handleSubmit(addUserHandler)} inline>
 
                     {submited && valid ? <Alert color="success">
                         Success
                     </Alert> : ""}
                     <Row form>
                         <Col md={6}>
-                            <h2>Add User</h2>
-
-
+                            <h2>Register</h2>
                             <FormGroup>
-                                <Label for="firstname" className="col-sm-2">FirstName</Label>
-
-                                <input type="text" className="form-control" id="username" value={enteredFirstName} placeholder="FirstName" onChange={firstNameNameHandler} required />
+                                <Label for="firstName" className="col-sm-2">FirstName</Label>
+          
+                                <input type="text" name="firstName" className="form-control" onChange={firstNameNameHandler} id="firstName" value={enteredFirstName} placeholder="FirstName"  {...register('firstName')} />
+                                <p  style={{color:'red'}} > {errors.firstName?.message} </p>
 
                             </FormGroup>
 
                             <FormGroup>
-                                <Label for="lastname" >Lastname</Label>
-                                <input type="text" className="form-control" id="lastname" value={enteredLastName} placeholder="Lastname" onChange={lastNameHandler} required />
+                                <Label for="lastName" >Lastname</Label>
+                                <input type="text" name="lastName" className="form-control" id="lastname" value={enteredLastName} onChange={lastNameHandler}  placeholder="Lastname" {...register('lastName')} />
+                                <p  style={{color:'red'}}> {errors.lastName?.message} </p>
+
                             </FormGroup>
 
                             <FormGroup>
                                 <Label for="email"  >Email</Label>
 
-                                <input type="email" className="form-control" id="email" value={enteredEmail} placeholder="email" onChange={emailHandler} required />
+                                <input type="email" name="email" className="form-control" id="email"  onChange={emailHandler} value={enteredEmail} placeholder="email"  {...register('email')} />
+                                <p  style={{color:'red'}}> {errors.email?.message} </p>
+
                             </FormGroup>
 
 
                             <FormGroup>
                                 <Label for="age" >Age</Label>
-                                <input type="number" className="form-control" id="age" value={enteredAge} placeholder="Age" onChange={ageHandler} required />
+                                <input type="number" name="age" className="form-control" id="age"  onChange={ageHandler}  value={enteredAge} placeholder="Age" {...register('age')} />
+                                <p  style={{color:'red'}}> {errors.age?.message} </p>
+
                             </FormGroup>
-                            <Button type="submit">Add User</Button>
+                            <Button type="submit">Register</Button>
                         </Col>
                     </Row>
 
